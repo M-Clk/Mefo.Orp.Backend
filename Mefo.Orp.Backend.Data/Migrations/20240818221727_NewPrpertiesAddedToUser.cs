2909592ea06a48a7dcd24680e83acf1402ff1de2
Muhammed Çelik <celikmuhammed21@hotmail.com>
2024-08-19 01:21:47 +0300

    add missed migration classes

---
+++ Mefo.Orp.Backend.Data/Migrations/20240818221727_NewPrpertiesAddedToUser.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mefo.Orp.Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewPrpertiesAddedToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "Users");
+        }
+    }
+}
