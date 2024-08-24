3864bf26697e831569739483e223bc9574a444e1
Muhammed Çelik <celikmuhammed21@hotmail.com>
2024-08-24 15:48:40 +0300

    Add generic repository pattern

---
+++ Mefo.Orp.Backend.Data/Repositories/Abstracts/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Mefo.Orp.Backend.Models.Entities.Abstracts;
+
+namespace Mefo.Orp.Backend.Data.Repositories.Abstracts
+{
+    public interface IRepository<T> where T : IBaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Mefo.Orp.Backend.Data/Repositories/Repository.cs
@@ -0,0 +1,20 @@
+﻿using Mefo.Orp.Backend.Data.Repositories.Abstracts;
+using Mefo.Orp.Backend.Models.Entities.Abstracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mefo.Orp.Backend.Data.Repositories;
+
+public class Repository<T>(DbContext dbContext) : IRepository<T> where T : class, IBaseEntity
+{
+    private readonly DbSet<T> _dbSet = dbContext.Set<T>();
+
+    public virtual async Task<T> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+
+    public virtual async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+    public virtual void Update(T entity) => _dbSet.Update(entity);
+
+    public virtual void Delete(T entity) => _dbSet.Remove(entity);
+}
